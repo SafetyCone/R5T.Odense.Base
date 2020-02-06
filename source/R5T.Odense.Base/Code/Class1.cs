a2a16b469b41bb6080da547ebd9018ef8f109a08
MinexAutomation <minex.automation@gmail.com>
2020-02-06 15:14:04 -0800

    Added IVisualStudioSolutionFileProjectTypeGuidProvider service definition interface type.

--- source/R5T.Odense.Base/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Odense.Base
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Odense.Base/Code/Services/Definitions/IVisualStudioSolutionFileProjectTypeGuidProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Odense
+{
+    public interface IVisualStudioSolutionFileProjectTypeGuidProvider
+    {
+        Guid GetNetStandardLibraryProjectTypeGuid()
+    }
+}
